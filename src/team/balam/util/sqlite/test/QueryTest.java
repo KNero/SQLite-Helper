fbd7aaef3b88ee7baf0821f5b72487f6b7f5ca7e
KwonSeongmin <kwonsm@icloud.com>
2017-05-03 18:22:14 +0900

    TestCode  추가.

--- src/team/balam/util/sqlite/test/QueryTest.java
+++ src/team/balam/util/sqlite/test/QueryTest.java
@@ -1,6 +1,7 @@
 package team.balam.util.sqlite.test;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,7 +17,7 @@ public class QueryTest
 {
 	private static final String DB_NAME = "./db/test.db";
 	private static final String DROP_TABLE_QUERY = "DROP TABLE IF EXISTS TEST";
-	private static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS TEST(DATA1 TEXT PRIMARY KEY, DATA2 NUMERIC, DATA3 INTEGER)";
+	private static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS TEST(DATA1 TEXT, DATA2 NUMERIC, DATA3 INTEGER, PRIMARY KEY (DATA1, DATA2))";
 	
 	public static void initTest() throws Exception
 	{
@@ -191,6 +192,121 @@ public class QueryTest
 			if(selectResult.isSuccess())
 			{
 				Assert.assertEquals(0, selectVo.getResult().getSelectResult().size());
+			}
+			else
+			{
+				throw selectResult.getException();
+			}
+		}
+		finally
+		{
+			if(selectResult != null)
+			{
+				selectResult.close();				
+			}
+		}
+	}
+	
+	@Test
+	public void testSelectTableMeta() throws Exception
+	{
+		initTest();
+		
+		Result selectResult = null;
+		
+		try
+		{
+			QueryVO selectVo = QueryVoFactory.createSelect();
+			selectVo.setQuery("PRAGMA table_info(test)");
+			
+			PoolManager.getInstance().executeQuery(DB_NAME, selectVo);
+			
+			selectResult = selectVo.getResult();
+			if(selectResult.isSuccess())
+			{
+				List<HashMap<String, Object>> list = selectResult.getSelectResult();
+				
+				Assert.assertEquals(3, list.size());
+				
+				Assert.assertEquals("DATA1", list.get(0).get("name"));
+				Assert.assertEquals("TEXT", list.get(0).get("type"));
+				
+				Assert.assertEquals("DATA2", list.get(1).get("name"));
+				Assert.assertEquals("NUMERIC", list.get(1).get("type"));
+				
+				Assert.assertEquals("DATA3", list.get(2).get("name"));
+				Assert.assertEquals("INTEGER", list.get(2).get("type"));
+			}
+			else
+			{
+				throw selectResult.getException();
+			}
+		}
+		finally
+		{
+			if(selectResult != null)
+			{
+				selectResult.close();				
+			}
+		}
+	}
+	
+	@Test
+	public void testSelectTableIndex() throws Exception
+	{
+		initTest();
+		
+		Result selectResult = null;
+		
+		try
+		{
+			QueryVO selectVo = QueryVoFactory.createSelect();
+			selectVo.setQuery("PRAGMA index_list(test)");
+			
+			PoolManager.getInstance().executeQuery(DB_NAME, selectVo);
+			
+			selectResult = selectVo.getResult();
+			if(selectResult.isSuccess())
+			{
+				if(selectResult.getSelectResult().size() > 0)
+				{
+					HashMap<String, Object> output = selectResult.getSelectResult().get(0);
+					System.out.println(output.get("seq")); //A sequence number assigned to each index for internal tracking purposes.
+					System.out.println(output.get("name")); //The name of the index.
+					System.out.println(output.get("unique")); //"1" if the index is UNIQUE and "0" if not.
+					System.out.println(output.get("origin")); //"c" if the index was created by a CREATE INDEX statement, "u" if the index was created by a UNIQUE constraint, or "pk" if the index was created by a PRIMARY KEY constraint.
+					System.out.println(output.get("partial")); //"1" if the index is a partial index and "0" if not.
+					
+					Result selectResult2 = null;
+					QueryVO selectVo2 = QueryVoFactory.createSelect();
+					selectVo2.setQuery("PRAGMA index_info(" + output.get("name") + ")");
+					
+					try
+					{
+						PoolManager.getInstance().executeQuery(DB_NAME, selectVo2);
+						selectResult2 = selectVo2.getResult();
+						if(selectResult2.isSuccess())
+						{
+							for(HashMap<String, Object> output2 : selectResult2.getSelectResult())
+							{
+								System.out.println(output2.get("seqno"));
+								System.out.println(output2.get("name"));
+								System.out.println(output2.get("cid"));
+							}
+						}
+						else
+						{
+							throw selectResult2.getException();
+						}
+					}
+					finally
+					{
+						if(selectResult != null)
+						{
+							selectResult2.close();				
+						}
+					}
+				}
 			}
 			else
 			{
